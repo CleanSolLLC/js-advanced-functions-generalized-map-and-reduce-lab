42a49807c093c6833d59d0667906d85c3e52e570
mamark90 <mamark90@gmail.com>
2021-08-14 15:31:21 -0400

    commit tree bb36b64d81f232ad9d89b634b5fc5800b63d1071

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your functions here
+function map(sourceArray, func) {
+  let newArray = [];	
+  for (let i = 0; i < sourceArray.length; i++) {
+	newArray.push(func(sourceArray[i]))
+  }  
+  return newArray;
+}
+
+
+function reduce (array, funcy, start) {
+  debugger;
+    let tmp;
+
+    if(start) {
+        tmp = start;
+        for (let i = 0; i < array.length; i++) {
+            tmp = funcy(array[i], tmp);
+        }
+    }
+
+    else {
+        tmp = array[0];
+        for (let i = 1; i < array.length; i++) {
+            tmp = funcy(array[i], tmp);
+        }
+    }
+    return tmp;
+}
+
+
+
+
